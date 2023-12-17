24996b90f87d87d10b46913ec1a4abd9b5ad4962
Евгений Щёголев <totenkopf@MacBook-Pro-Evgenij.local>
2023-12-17 14:08:00 +0300

    почистил код

--- src/components/Game/game.jsx
+++ src/components/Game/game.jsx
@@ -6,16 +6,21 @@ import Step from '../Step/step';
 import Toggle from '../Toggle/toggle';
 
 const Game = () => {
-
+    //стейт доски создаем массив из девяти ячеек
     const [board, setBoard] = useState(Array(9).fill(null));
+    // определяем чей ход
     const [xIsNext, setXIsNext] = useState(true);
+    // определение победителя
     const winner = calculateWinner(board);
+    // стейт темы два значения 
     const [theme, setTheme] = useState("dark");
 
+    // Изменение темы
     const switchTheme = () => {
         setTheme((cur) => (cur === 'light' ? 'dark' : 'light'));
     };
 
+    // определяем клип по ячейке и чей ход
     const handleClick = (index) =>{
         const boardCopy = [...board]
         // Определить был ли клик по ячейке или игра закончилась
@@ -26,7 +31,7 @@ const Game = () => {
         setBoard(boardCopy)
         setXIsNext(!xIsNext)
     }
-
+    // Очищает поле
     const startNewGame = () => {
         return (
             <button className='start__btn' onClick={() => setBoard(Array(9).fill(null))}>Новая игра</button>
